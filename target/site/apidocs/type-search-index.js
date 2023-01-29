17bc2a776dab0307674d49e6993335eb573d4ba4
CWillR <connerreedwilliams@gmail.com>
2023-01-28 20:48:42 -0600

    Simple program to draw

---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.team5.blankslate","l":"BlankSlate"},{"p":"com.team5.blankslate","l":"DrawArea"}];updateSearchResults();
