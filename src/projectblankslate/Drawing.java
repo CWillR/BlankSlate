9241656e4dae1ca9d98b2b7d74f188a2cf38d864
dukkdukk <123915062+dukkdukk@users.noreply.github.com>
2023-04-20 00:10:48 -0500

    Update Drawing.java

--- src/projectblankslate/Drawing.java
+++ src/projectblankslate/Drawing.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package projectblankslate;
 
 import java.awt.BasicStroke;
@@ -17,12 +14,14 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
+
+/**
+* Represents a component that allows the user to draw on it with various tools such as a pen, rectangle, or circle.
+* Inherits from JComponent.
+*/
 public class Drawing extends JComponent {
     
     //Image we'll be drawing in
@@ -32,15 +31,36 @@ public class Drawing extends JComponent {
     //Mouse coordinates
     private int currentX, currentY, oldX, oldY, thickness = 3;
     
-    // NOTE: NEW CODE
+    /**
+    * Flags indicating whether the user is drawing a rectangle or square.
+    */
     public boolean isDrawingRectangle, isDrawingCircle = false;
-    private int startX, startY, endX, endY;
 
+    /**
+    * The x-coordinate and y-coordinate of the starting point of the rectangle or circle.
+    */
+    private int startX, startY;
+
+    /**
+    * The x-coordinate and y-coordinate of the ending point of the rectangle or circle.
+    */
+    private int endX, endY;
+
+    /**
+    * Constructor for the Drawing object.
+    * Initializes the object and adds mouse listeners to detect mouse events.
+    */
     public Drawing() {
         setDoubleBuffered(false);
         
         addMouseListener(new MouseAdapter() {
-
+            /**
+            * Called when the user presses a mouse button.
+            * Saves the x and y coordinates of the mouse and sets them as the old coordinates.
+            * Also saves the starting x and y coordinates of the shape.
+            * 
+            * @param e The MouseEvent that triggered this method.
+            */
             public void mousePressed(MouseEvent e) {
                 //save coord x,y when mouse is pressed
                 oldX = e.getX();
@@ -49,6 +69,13 @@ public class Drawing extends JComponent {
                 startY = e.getY();
             }
             
+            /**
+            * Called when the user releases a mouse button.
+            * Saves the x and y coordinates of the mouse and sets them as the ending coordinates.
+            * Then, if the user is drawing a rectangle or circle, draws the shape on the screen.
+            * 
+            * @param e The MouseEvent that triggered this method.
+            */
             public void mouseReleased(MouseEvent e) {
                 // save coordinates of end point
                 endX = e.getX();
@@ -66,12 +93,18 @@ public class Drawing extends JComponent {
                         g2.fillOval(shapeX, shapeY, shapeWidth, shapeHeight);
                         repaint();
                     }
-            }
+                }
     
         });
         
         addMouseMotionListener(new MouseMotionAdapter() {
-
+            /**
+            * Called when the user drags the mouse.
+            * If the user is drawing a rectangle or circle, updates the ending coordinates of the shape.
+            * Otherwise, draws a line connecting the old and new coordinates.
+            * 
+            * @param e The MouseEvent that triggered this method.
+            */
             public void mouseDragged(MouseEvent e) {
                 if (isDrawingRectangle) {
                     // update coordinates of rectangle end point
@@ -101,19 +134,33 @@ public class Drawing extends JComponent {
             } 
         }); 
     }
-    // END NEW CODE
-            
+    
+        
+    /**
+    * Converts the given Image to a BufferedImage.
+    * 
+    * @param image The Image to convert.
+    * @return The BufferedImage representation of the given Image.
+    */
     public static BufferedImage toBufferedImage(Image image) {
         if(image instanceof BufferedImage) {
             return (BufferedImage) image;
-        }   
-        BufferedImage bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);   
+        }
+            
+        BufferedImage bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+            
         Graphics2D bGr = bimage.createGraphics();
         bGr.drawImage(image, 0, 0, null);
-        bGr.dispose();  
+        bGr.dispose();
+            
         return bimage;
         }
     
+    /**
+    * Paints this component by drawing the stored image and any shapes currently being drawn on top of it.
+    * 
+    * @param g The Graphics object to use for painting.
+    */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -125,7 +172,6 @@ public class Drawing extends JComponent {
             //enable antialiasing
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                     RenderingHints.VALUE_ANTIALIAS_ON);
-            g2.setStroke(new BasicStroke(5));
             clear();
         }
 
@@ -134,32 +180,38 @@ public class Drawing extends JComponent {
         if (isDrawingRectangle) {
             // draw the rectangle as the user drags the mouse
             Graphics2D g2d = (Graphics2D) g;
-            g2d.setStroke(new BasicStroke(3));
+            g2d.setStroke(new BasicStroke(thickness));
             g2d.drawRect(Math.min(startX, endX), Math.min(startY, endY),
                     Math.abs(endX - startX), Math.abs(endY - startY));
         } else if (isDrawingCircle) {
             // draw the rectangle as the user drags the mouse
             Graphics2D g2d = (Graphics2D) g;
-            g2d.setStroke(new BasicStroke(3));
+            g2d.setStroke(new BasicStroke(thickness));
             g2d.drawOval(Math.min(startX, endX), Math.min(startY, endY),
                     Math.abs(endX - startX), Math.abs(endY - startY));
         }
     } 
     
-    //Change the color of the pen (g2) to that of the user's choice
+    /**
+    * Prompts the user to choose a color and sets the pen color to the chosen color.
+    */    
     public void colorChooser() {
         Color color = JColorChooser.showDialog(null, "Choose Color", Color.BLACK);
         g2.setPaint(color);
     }
     
-    //Increases the thickness of the pen by 1
+    /**
+    * Increases the thickness of the pen by 1.
+    */    
     public void increaseThick() {
         g2.setStroke(new BasicStroke(thickness++));
         //Have to set stroke again, otherwise g2 will be one value of thickness behind
         g2.setStroke(new BasicStroke(thickness));
     }
     
-    //Decreases the thickness of the pen by 1 unless it already equals 1, in which it does nothing to prevent errors
+    /**
+    * Decreases the thickness of the pen by 1, unless the thickness is already 1, in which case it does nothing.
+    */    
     public void decreaseThick() {
         if (thickness==1) { 
             g2.setStroke(new BasicStroke(thickness));
@@ -171,99 +223,55 @@ public class Drawing extends JComponent {
         g2.setStroke(new BasicStroke(thickness));
     }
     
-    //Define what the buttons do here
+    /**
+    * Clears the drawing area by filling it with white.
+    */    
     public void clear() {  
         g2.setPaint(Color.white);
         //draw white on entire draw area to clear
         g2.fillRect(0, 0, getSize().width, getSize().height);
         g2.setPaint(Color.black);
-        g2.setStroke(new BasicStroke(3));
-        thickness = 3;
-        drawLine();
+        g2.setStroke(new BasicStroke(1));
         repaint();
     }
     
+    /**
+    * Saves the current image as a JPEG file and allows user to create a name for file.
+    */
     public void save() {
-        String fileName;
-        JFrame namePopup = new JFrame();
-        fileName = JOptionPane.showInputDialog(namePopup,"Name your drawing (leaving the response blank or naming file 'null' will not save your drawing)", "Save",JOptionPane.INFORMATION_MESSAGE);
-        File tmpDir = new File("Drawings//" + fileName + ".jpg");
-        boolean exists = tmpDir.exists();
-        if (exists == true){
-            int i = 1;
-            while (i==1){
-                JFrame overwritePopup = new JFrame();
-                int result = JOptionPane.showConfirmDialog(overwritePopup, 
-                    "That file name already exists. Would you like to overwrite it?",
-                    "Overwrite File?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-                if(result == JOptionPane.YES_OPTION){
-                    i = 0;
-                }
-                if(result == JOptionPane.NO_OPTION) {
-                    fileName = JOptionPane.showInputDialog(namePopup,"Name file");
-                    tmpDir = new File("Drawings//" + fileName + ".jpg");
-                    exists = tmpDir.exists();
-                    if (exists == true) {
-                        i = 1;
-                    }
-                    if (exists == false) {
-                        i = 0;
-                    }
-                }
-            }
-        }
-        if ((fileName != null) && (fileName.length()>0)) {
-            try {
-                BufferedImage bi = toBufferedImage(image);
-                File outputfile = new File("Drawings//" + fileName + ".jpg");
-                ImageIO.write(bi, "jpg", outputfile);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } 
-        }  
+        try {
+            BufferedImage bi = toBufferedImage(image);
+            File outputfile = new File("test.jpg");
+            ImageIO.write(bi, "jpg", outputfile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }        
     } 
     
-    // Draw rectangle
+    /**
+    * Sets the component to draw rectangles.
+    */    
     public void drawRect(){
         isDrawingRectangle = true;
         isDrawingCircle = false;
     }
     
-    // Draw Cirlce
+    /**
+    * Sets the component to draw circles.
+    */
     public void drawCircle(){
         isDrawingCircle = true;
         isDrawingRectangle = false;
     }
     
-    // Draw Line
+    /**
+    * Sets the component to draw lines.
+    */
     public void drawLine() {
         isDrawingRectangle = false;
         isDrawingCircle = false;
     }
-    //Help button pop up.
-    public static void help() {
-        JOptionPane.showMessageDialog(null, """
-                                                                       Welcome to Blank Slate!
-                                            This tutorial will help you understand the features present in this application 
-                                            Line: This tool draws lines anywhere on the canvas by holding down on the left 
-                                            mouse button.
-                                            Rectangle: This tool draws a rectangle infilled with selected color starting 
-                                            from the point you click the left mouse button and connecting to where you 
-                                            release the left mouse button. Also has a preview available for the rectangle 
-                                            you will draw upon release. Furthermore this is the deafault tool.
-                                            Circle: This tool draws a circle infilled with selected color starting from the 
-                                            point you click the left mouse button and looping to where you release the left
-                                            mouse button. Also has a preview available for the circle you will draw upon 
-                                            release.
-                                            Change Color: This button opens the color changing menu complete with swatches, 
-                                            RGB, CMYK and more. Also Has a preview available for color selection. 
-                                            Default is black.
-                                            Thicker Pen: Thickens pen width by 1. Default is 3.
-                                            Thinner Pen: Thins pen width by 1. Default is 3.
-                                            Clear: Returns tool, color and pen to default as well as clearing canvas. 
-                                            Save Drawing: Captures your masterpiece in its entirety and saves it to the 
-                                            drawings folder inside of the black slate app.
-                                            Help: This page right here.""" , "Tool Tips", JOptionPane.INFORMATION_MESSAGE);
-    }
 }
 
+
+   
